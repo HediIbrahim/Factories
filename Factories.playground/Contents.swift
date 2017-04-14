16756c1276849b7b29d4cdfa75e92a2dff9ea673
Hedi <HediIbrahim@users.noreply.github.com>
2017-04-14 14:47:48 -0400

    Add files via upload

---
+++ Factories.playground/Contents.swift
@@ -0,0 +1,62 @@
+let favoriteColor = "Blue"
+
+let myNewFavColor: String = "Red" //We have created a constant variable  called myNewFavColor of type String and assigned it the value "Red". Explicit with type info shown.
+
+let question = "What do you want to eat for dinner?"
+
+var answer: String
+answer = "Chicken with mashed potatoes"
+print(answer)
+
+/// INT TYPE
+
+var age = 11
+
+age + 10
+
+age / 2
+
+age - 67
+
+age * (-4)
+
+-4 * age
+
+2017 + 2016
+
+var thisYear = "2017"
+var lastYear = "2016"
+var favEmoji = "🦄"
+var electionYear: Int
+electionYear = 2016
+
+thisYear + lastYear
+favEmoji + lastYear
+
+var firstPresinebtalTerm = 2016 + 4
+var middleSchoolGraduation: Int = 2016 + 3
+var nextElection = 2016 + 4
+
+
+age + firstPresinebtalTerm
+
+age + middleSchoolGraduation
+
+age + nextElection
+
+///  DOUBLE  TYPE
+
+let temperatureInF = 85.2 // Type inference. Tooltip shows Double Type.
+
+let heightInCM: Double = 187.96 // declaration of the constant
+
+// We've created a constant, called heightInCM, the Double type and assigned it a value of 187.96
+
+var dogs = "Poodles"
+var cats = "Scottish Fold"
+
+var sentance = " My favorite pets are \(dogs) and \(cats)"
+print(sentance)
+
+
+
